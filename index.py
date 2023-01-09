40ba83a5efeaf5e505e73874ef288aa94170fc5e
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-01-08 23:06:43 -0300

    Menu
    Menu com nove opções para acessar o sistema de notas.

--- index.py
+++ index.py
@@ -98,3 +98,21 @@ def mediaTurma(qtdAlunos):
         nota += dados[i]['nota']
     media = (nota/qtdAlunos)
     print(f'A média da turma é: {media:,.2f}')
+
+#Menu: 
+loop = 1
+while loop == 1:
+    print('\n ========================================================MENU==========================================================================================\n',
+        '| Selecione uma das opções abaixo:                                                                                                                   |\n',
+        '| Digite 1 para ver a lista em ordem alfabética dos alunos com o número de matricula e a sua nota.                                                   |\n',
+        '| Digite 2 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota.                                     |\n',
+        '| Digite 3 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos aprovados (>= 7.0).   |\n',
+        '| Digite 4 para ver a lista em ordem decrescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos reprovados (< 7.0). |\n',
+        '| Digite 5 para ver o percentual de aprovação, sabendo-se que a nota mínima para aprovação é >= 7.0.                                                 |\n',
+        '| Digite 6 para ver a nota que teve a maior frequência absoluta.                                                                                     |\n',
+        '| Digite 7 para ver o aluno com a maior nota (nome, matricula e nota).                                                                               |\n',
+        '| Digite 8 para ver o aluno com a menor nota (nome, matricula e nota).                                                                               |\n',
+        '| Digite 9 para ver a media da turma.                                                                                                                |\n',
+        '======================================================================================================================================================')
+    opcao = int(input())
+    print('\n')
