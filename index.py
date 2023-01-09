faa50a183d1f27538236a2de8f40362363934ad8
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-01-08 21:26:29 -0300

    Informações do Alunos
    Função para armazenar as informações do aluno na variável dados.

--- index.py
+++ index.py
@@ -1,3 +1,16 @@
+gabarito = input('Digite o gabarito da prova: ').lower()
+qtdAlunos = int(input('Digite a quantidade de alunos: '))
+dados = list()
+
+#função para receber as informaçoes do aluno e enviar para a lista.   
+def infoAlunos(qtdAlunos, gabarito):
+    for i in range(qtdAlunos):
+        matricula = int(input('Matricula do Aluno: '))
+        nome = str(input('Nome do aluno: ').lower())
+        resp = str(input('Resposta do aluno: ').lower())
+        nota = calcularNota(resp, gabarito)
+        dados.append({'nome': nome, 'matricula': matricula, 'nota': nota})
+
 #função para calcular quantas questões o aluno acertou
 
 def calcularNota(resp, gaba):
