2338d6d466e12be18d8ef4f760bb3d1afd894a6e
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-01-17 14:46:30 -0300

    Função menu
    Criação da função menu e correção da formatação.

--- texto.py
+++ texto.py
@@ -1,14 +1,17 @@
-menu = '''
-\n ========================================================MENU==========================================================================================\n',
-        '| Selecione uma das opções abaixo:                                                                                                                   |\n',
-        '| Digite 1 para ver a lista em ordem alfabética dos alunos com o número de matricula e a sua nota.                                                   |\n',
-        '| Digite 2 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota.                                     |\n',
-        '| Digite 3 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos aprovados (>= 7.0).   |\n',
-        '| Digite 4 para ver a lista em ordem decrescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos reprovados (< 7.0). |\n',
-        '| Digite 5 para ver o percentual de aprovação, sabendo-se que a nota mínima para aprovação é >= 7.0.                                                 |\n',
-        '| Digite 6 para ver a nota que teve a maior frequência absoluta.                                                                                     |\n',
-        '| Digite 7 para ver o aluno com a maior nota (nome, matricula e nota).                                                                               |\n',
-        '| Digite 8 para ver o aluno com a menor nota (nome, matricula e nota).                                                                               |\n',
-        '| Digite 9 para ver a media da turma.                                                                                                                |\n',
-        '======================================================================================================================================================
-'''
+
+def menu():
+        print('=' * 72, 'Menu', '=' * 75)
+        print('|', ' ' *149, '|')
+        print('| Selecione uma das opções abaixo:', ' '*116,'|\n|',' ' *149, '|')
+        print('| Digite 1 para ver a lista em ordem alfabética dos alunos com o número de matricula e a sua nota.', ' '*52,'|\n|',' ' *149, '|')
+        print('| Digite 2 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota.', ' '*38,'|\n|',' ' *149, '|')
+        print('| Digite 3 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos aprovados (>= 7.0).', ' '*4,'|\n|',' ' *149, '|')
+        print('| Digite 4 para ver a lista em ordem decrescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos reprovados (< 7.0).', ' '*2,'|\n|',' ' *149, '|')
+        print('| Digite 5 para ver o percentual de aprovação, sabendo-se que a nota mínima para aprovação é >= 7.0.', ' '*50,'|\n|',' ' *149, '|')
+        print('| Digite 6 para ver a nota que teve a maior frequência absoluta.', ' '*86,'|\n|',' ' *149, '|')
+        print('| Digite 7 para ver o aluno com a maior nota (nome, matricula e nota).', ' '*80,'|\n|',' ' *149, '|')  
+        print('| Digite 8 para ver o aluno com a menor nota (nome, matricula e nota).', ' '*80,'|\n|',' ' *149, '|')
+        print('| Digite 9 para ver a media da turma.', ' '*113,'|\n|',' ' *149, '|')
+        print('=' * 153)
+        return
+      
