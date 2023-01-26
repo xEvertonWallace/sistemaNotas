90a0b8efeeb30cce6c9ecd59e4e34a7c07f354d9
xEvertonWallace <102435699+xEvertonWallace@users.noreply.github.com>
2023-01-25 22:28:17 -0300

    renomeação arquivo de texto do menu
    retirada da função menu  e substituição por uma variavel com o texto.

---
+++ textoMenu.py
@@ -0,0 +1,25 @@
+menu = f''' 
+    {'=' * 72} Menu {'=' * 75} 
+    |{' ' *151}| 
+    | Selecione uma das opções abaixo: {' '*117}| 
+    |{' ' *151}| 
+    | Digite 1 para ver a lista em ordem alfabética dos alunos com o número de matricula e a sua nota. {' '*53}| 
+    |{' ' *151}| 
+    | Digite 2 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota.{' '*40}| 
+    |{' ' *151}| 
+    | Digite 3 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos aprovados (>= 7.0). {' '*5}| 
+    |{' ' *151}| 
+    | Digite 4 para ver a lista em ordem decrescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos reprovados (< 7.0). {' '*3}|  
+    |{' ' *151}|  
+    | Digite 5 para ver o percentual de aprovação, sabendo-se que a nota mínima para aprovação é >= 7.0. {' '*51}| 
+    |{' ' *151}| 
+    | Digite 6 para ver a nota que teve a maior frequência absoluta. {' '*87}| 
+    |{' ' *151}| 
+    | Digite 7 para ver o aluno com a maior nota (nome, matricula e nota). {' '*81}| 
+    |{' ' *151}| 
+    | Digite 8 para ver o aluno com a menor nota (nome, matricula e nota). {' '*81}| 
+    |{' ' *151}| 
+    | Digite 9 para ver a media da turma. {' '*114}| 
+    |{' ' *151}| 
+    {'='*153} 
+'''
